695ffb6da0a5347263a2d83926e338081e35f58c
rano <hernawan.rano@gmail.com>
2026-04-22 13:42:35 +0700

    feat: connect to railway backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 // apps/customer-app/src/services/api.js
-const API_BASE = 'http://localhost:5000/api';
-
+const API_BASE = 'https://zippy-commitment.up.railway.app/api';
 export async function fetchAPI(url, options = {}) {
   try {
     const res = await fetch(`${API_BASE}${url}`, {
